5504b7864f5a7637d7414bb426815eac0513c233
Len <len@purchasingplatform.com>
2026-06-30 13:13:41 -0500

    Add files via upload

---
+++ _old/index.php
@@ -0,0 +1,24 @@
+<html>
+<title>Welcome to your new hosting plan!</title>
+<body>
+
+<br/>
+<center><img src="http://manufaxure.com/manufaxure_logo.jpg" /></center>
+<br/>
+
+<p>
+ Welcome to your new <a href="http://manufaxure.com">Manufaxure Corp.</a>
+ hosting plan.
+</p>
+<p>
+ Use your user account name and password to log into your <a href="http://manufaxure.com/control-panel">control panel</a>, and get started.
+</p>
+<p>
+ Questions or comments? Contact us for <a href="http://manufaxure.com/support">technical support</a>.
+</p>
+
+<br/>
+<center><img src="http://manufaxure.com/manufaxure_logo.jpg" /></center>
+
+</body>
+</html>
